a62598fba2591ef30ae9c85271fc7d444ba23950
Roblox-user <qumu.idktto@gmail.com>
2025-02-09 16:25:13 +0800

    更新 start.lua

--- start.lua
+++ start.lua
@@ -12,24 +12,7 @@ local function Notify(Text)
 	})
 end
 
-local PlaceName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
 
-getgenv().PlaceName = PlaceName
-
-PlaceName = PlaceName:gsub("%b[]", "")
-PlaceName = PlaceName:gsub("[^%a]", "")
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Analytics.lua"))()
-
-local Code: string = game:HttpGet(`https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Games/{PlaceName}.lua`)
-
-if Code ~= "404: Not Found" then
-	Notify("找不到游戏,无法加载.")
-	getgenv().PlaceFileName = PlaceName
-else
 	Notify("游戏已获取 用时:", `Loaded in {math.floor((tick() - StartLoadTime) * 10) / 10}s`, ",响应中…")
-	getgenv().ScriptVersion = "Universal"
-	Code = game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Core.lua")
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-user/Dig-it/refs/heads/main/Main%20game.lua"))()
 end
-
-getgenv().FrostByteHandleFunction(loadstring(Code))
